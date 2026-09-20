a71631795a46b27deceb7c0ce109118aa1a82b11
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2026-09-20 12:21:58 +0200

    Generate an icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.amedeoalf.betterchrono"
         minSdk = 30
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
