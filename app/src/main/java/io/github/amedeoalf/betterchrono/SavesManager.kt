f2f7fb0b572f6ca2ed89bd09bfe4c5bfe69c4bee
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2026-09-23 22:21:55 +0200

    Sort saves based on time

--- app/src/main/java/io/github/amedeoalf/betterchrono/SavesManager.kt
+++ app/src/main/java/io/github/amedeoalf/betterchrono/SavesManager.kt
@@ -6,7 +6,7 @@ class SavesManager(private val saveDir: File) {
     fun save(vm: ChronoViewModel, name: String) =
         saveDir.resolve(name).writer().use { vm.export(it) }
 
-    fun listSaves() = saveDir.listFiles()?.map { it.name } ?: emptyList()
+    fun listSaves() = saveDir.listFiles()!!.sortedByDescending { it.lastModified() }
 
     fun loadSave(vm: ChronoViewModel, name: String) =
         saveDir.resolve(name).reader().use { vm.import(it) }
