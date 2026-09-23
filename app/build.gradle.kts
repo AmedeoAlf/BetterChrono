bedc67fa9ff392d2b26e33226ea0a68adf88e9be
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2026-09-23 22:22:47 +0200

    Bump version to v0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.amedeoalf.betterchrono"
         minSdk = 30
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
