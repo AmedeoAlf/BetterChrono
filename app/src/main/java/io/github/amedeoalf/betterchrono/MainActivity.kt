2c3e9b8967fdf227fc921c10414e51e8f3838fd5
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2026-09-23 21:38:30 +0200

    Add a save dialog

--- app/src/main/java/io/github/amedeoalf/betterchrono/MainActivity.kt
+++ app/src/main/java/io/github/amedeoalf/betterchrono/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
@@ -20,13 +21,18 @@ import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -39,6 +45,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.isUnspecified
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.amedeoalf.betterchrono.ui.theme.BetterChronoTheme
 import java.io.File
@@ -143,34 +150,40 @@ fun TimeDisplay(timeMs: Long, isPaused: Boolean) {
 @Composable
 fun Screen(
     vm: ChronoViewModel = viewModel(),
-    savesManager: SavesManager = SavesManager(File("."))
+    savesManager: SavesManager = SavesManager(File(""))
 ) {
     val events = remember { vm.events }
+    val showSaveDialog = remember { mutableStateOf(false) }
     Surface(
         Modifier
             .fillMaxSize()
     ) {
-        Column(
-            Modifier.safeDrawingPadding(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(5.dp)
-        ) {
-            val displayInfo = vm.displayInfo
-            ButtonBar(vm)
-            TimeDisplay(displayInfo.currTime, displayInfo.paused)
+        Box(contentAlignment = Alignment.Center) {
             Column(
                 Modifier
-                    .padding(
-                        WindowInsets.safeContent.only(WindowInsetsSides.Horizontal)
-                            .asPaddingValues()
-                    ),
+                    .fillMaxSize()
+                    .safeDrawingPadding(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(5.dp)
             ) {
-                EditingWidget(
-                    events,
-                    { savesManager.save(vm, "test") },
-                    { savesManager.loadSave(vm, "test") })
-                LapDisplay(displayInfo.laps, displayInfo.currLap)
+                val displayInfo = vm.displayInfo
+                ButtonBar(vm)
+                TimeDisplay(displayInfo.currTime, displayInfo.paused)
+                Column(
+                    Modifier
+                        .padding(
+                            WindowInsets.safeContent.only(WindowInsetsSides.Horizontal)
+                                .asPaddingValues()
+                        ),
+                ) {
+                    EditingWidget(
+                        events,
+                        { showSaveDialog.value = true },
+                        { savesManager.loadSave(vm, "test") })
+                    LapDisplay(displayInfo.laps, displayInfo.currLap)
+                }
             }
+            SaveDialog(showSaveDialog) { savesManager.save(vm, it.toString()) }
         }
     }
 }
@@ -228,6 +241,63 @@ fun ButtonBar(vm: ChronoViewModel) {
         ) { vm.events.clear() }
         ChronoBtn("Ferma", "vol +") { vm.addStopEvent() }
         ChronoBtn("Avvia/Giro", "vol -") { vm.addStartEvent() }
+    }
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SaveDialog(
+    shown: MutableState<Boolean> = mutableStateOf(true),
+    save: (filename: CharSequence) -> Unit = {}
+) {
+    var shown by shown
+    if (shown) {
+        Dialog({ shown = false }) {
+            Card(
+                colors = CardDefaults.cardColors()
+                    .copy(containerColor = MaterialTheme.colorScheme.surfaceContainerLow),
+                elevation = CardDefaults.elevatedCardElevation()
+            ) {
+                Column(
+                    Modifier.padding(15.dp),
+                    verticalArrangement = Arrangement.spacedBy(10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(
+                        "Salva",
+                        style = MaterialTheme.typography.titleLarge,
+                    )
+                    Text(
+                        "Recupera questi tempi in un secondo momento",
+                        textAlign = TextAlign.Center
+                    )
+                    val tf = rememberTextFieldState()
+                    TextField(
+                        tf,
+                        label = { Text("Nome salvataggio") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(5.dp, Alignment.End),
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Button(
+                            { shown = false },
+                            colors = ButtonDefaults.textButtonColors()
+                        ) { Text("Annulla") }
+                        Button(
+                            {
+                                save(tf.text)
+                                shown = false
+                            },
+                            enabled = tf.text.isNotEmpty(),
+                            colors = ButtonDefaults.textButtonColors()
+                        ) { Text("Salva") }
+                    }
+                }
+            }
+        }
     }
 
 }
